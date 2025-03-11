b5c99ac4da46478711975fbf19aa30d967df2dc3
Task <47542160+taskmasterpeace@users.noreply.github.com>
2025-03-11 11:39:23 -0400

    Fixed repo to only track meetng

--- services/base_service.py
+++
@@ -1,9 +0,0 @@
-class TranscriptionService:
-    def __init__(self):
-        pass
-        
-    def setup(self, api_key):
-        raise NotImplementedError
-        
-    def transcribe(self, file_path, config=None):
-        raise NotImplementedError
